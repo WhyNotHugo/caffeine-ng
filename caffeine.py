4afdaec2db133ae49868c12c397fd219ac0f7850
Brad Smith <>
2009-07-03 15:22:05 -0400

    Made a slight modification that cancels the 'timed activation' mode when the user toggles Caffeine off. Also added a few logging messages to describe which timer-related operations are taking place. Lastly, I decided to remove all tabs from the file. The official Python style guide recommends using either tabs or spaces, but not both, so I replaced all tabs with 4 spaces. I'm open to alternative opinions though.

--- caffeine.py
+++ caffeine.py
@@ -7,15 +7,15 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 
 import os, sys
 import gtk
@@ -29,30 +29,31 @@ FULL_ICON_PATH = os.path.abspath(os.path.join(os.path.split(__file__)[0], "Full_
 
 sleepPrevented = False
 screenSaverCookie = None
+timer = None
 
 # GUI callbacks
 def sleepPreventionPressed(widget, data = None):
-	global sleepPrevented
-	if sleepPrevented:
-		widget.set_from_file(EMPTY_ICON_PATH)
-	else:
-		widget.set_from_file(FULL_ICON_PATH)
-	toggleSleepPrevention()
+    global sleepPrevented
+    if sleepPrevented:
+        widget.set_from_file(EMPTY_ICON_PATH)
+    else:
+        widget.set_from_file(FULL_ICON_PATH)
+    toggleSleepPrevention()
 
 def showMenu(widget, button, time, data = None):
-	if button == 3 and data != None:
-		data.show_all()
-		data.popup(None, None, None, 3, time)
+    if button == 3 and data != None:
+        data.show_all()
+        data.popup(None, None, None, 3, time)
 
 def displayAboutBox(widget, data = None):
-	about = gtk.AboutDialog()
-        about.set_program_name("Caffeine")
-        about.set_version(VERSION_STRING)
-        about.set_copyright("Copyright © 2009 Brad Smith")
-        about.set_icon(gtk.gdk.pixbuf_new_from_file(FULL_ICON_PATH))
+    about = gtk.AboutDialog()
+    about.set_program_name("Caffeine")
+    about.set_version(VERSION_STRING)
+    about.set_copyright("Copyright © 2009 Brad Smith")
+    about.set_icon(gtk.gdk.pixbuf_new_from_file(FULL_ICON_PATH))
 
-        about.set_logo(gtk.gdk.pixbuf_new_from_file_at_size(FULL_ICON_PATH, 48, 48))
-        about.set_comments("""An application to temporarily prevent the activation of both the screen saver and the "sleep" powersaving mode.
+    about.set_logo(gtk.gdk.pixbuf_new_from_file_at_size(FULL_ICON_PATH, 48, 48))
+    about.set_comments("""An application to temporarily prevent the activation of both the screen saver and the "sleep" powersaving mode.
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
@@ -60,83 +61,95 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 
 You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """)
-        about.set_website("http://pragmattica.wordpress.com")
-        about.run()
-        about.destroy()
+    about.set_website("http://pragmattica.wordpress.com")
+    about.run()
+    about.destroy()
 
 def quitButtonPressed(widget, data = None):
-	gtk.main_quit()
-	quitCaffeine()
+    gtk.main_quit()
+    quitCaffeine()
 
 # Other procedures
 def quitCaffeine():
-	"""Perform final operations before program termination."""
-	global sleepPrevented
-	if sleepPrevented:
-		toggleSleepPrevention()
-	print "Caffeine exiting"
+    """Perform final operations before program termination."""
+    global sleepPrevented
+    if sleepPrevented:
+        toggleSleepPrevention()
+    print "Caffeine exiting"
 
 def toggleSleepPrevention():
-	global sleepPrevented, screenSaverCookie
-	bus = dbus.SessionBus()
-	if sleepPrevented:
-		ssProxy = None
-		if 'org.gnome.ScreenSaver' in bus.list_names(): # For Gnome
-			ssProxy = bus.get_object('org.gnome.ScreenSaver', '/org/gnome/ScreenSaver')
-		elif 'org.freedesktop.ScreenSaver' in bus.list_names(): # For KDE and others
-			ssProxy = bus.get_object('org.freedesktop.ScreenSaver', '/ScreenSaver')
-		else:
-			errMsg = "Error: couldn't find bus to allow re-enabling of the screen saver.\n\n" \
-				"Please visit the web-site listed in the 'About' dialog of this application " \
-				"and check for a newer version of the software."
-			errDlg = gtk.MessageDialog(type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_OK, message_format=errMsg)
-			errDlg.run()
-			errDlg.destroy()
-			sys.exit(1)
+    global sleepPrevented, screenSaverCookie, timer
+    bus = dbus.SessionBus()
+    if sleepPrevented:
+        ssProxy = None
+        if 'org.gnome.ScreenSaver' in bus.list_names(): # For Gnome
+            ssProxy = bus.get_object('org.gnome.ScreenSaver', '/org/gnome/ScreenSaver')
+        elif 'org.freedesktop.ScreenSaver' in bus.list_names(): # For KDE and others
+            ssProxy = bus.get_object('org.freedesktop.ScreenSaver', '/ScreenSaver')
+        else:
+            errMsg = "Error: couldn't find bus to allow re-enabling of the screen saver.\n\n" \
+                "Please visit the web-site listed in the 'About' dialog of this application " \
+                "and check for a newer version of the software."
+            errDlg = gtk.MessageDialog(type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_OK, message_format=errMsg)
+            errDlg.run()
+            errDlg.destroy()
+            sys.exit(1)
 
-		if screenSaverCookie != None:
-			ssProxy.UnInhibit(screenSaverCookie)
-		sleepPrevented = False
-		print "Caffiene is now dormant; powersaving is re-enabled"
-	else:
-		ssProxy = None
-		if 'org.gnome.ScreenSaver' in bus.list_names(): # For Gnome
-			ssProxy = bus.get_object('org.gnome.ScreenSaver', '/org/gnome/ScreenSaver')
-		elif 'org.freedesktop.ScreenSaver' in bus.list_names(): # For KDE and others
-			ssProxy = bus.get_object('org.freedesktop.ScreenSaver', '/ScreenSaver')
-		else:
-			errMsg = "Error: couldn't find bus to allow inhibiting of the screen saver.\n\n" \
-				"Please visit the web-site listed in the 'About' dialog of this application " \
-				"and check for a newer version of the software."
-			errDlg = gtk.MessageDialog(type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_OK, message_format=errMsg)
-			errDlg.run()
-			errDlg.destroy()
-			sys.exit(2)
+        if screenSaverCookie != None:
+            ssProxy.UnInhibit(screenSaverCookie)
+        sleepPrevented = False
+        print "Caffiene is now dormant; powersaving is re-enabled"
 
-		screenSaverCookie = ssProxy.Inhibit("Caffeine", "User has requested that Caffeine disable the screen saver")
-		sleepPrevented = True
-		print "Caffiene is now preventing powersaving modes and screensaver activation"
+        # If the user clicks on the full coffee-cup to disable sleep prevention, it should also
+        # cancel the timer for timed activation.
+        if timer != None:
+        	print "Cancelling the 'timed activation' timer (was set for " + str(timer.interval) + " seconds)"
+        	timer.cancel()
+    else:
+        ssProxy = None
+        if 'org.gnome.ScreenSaver' in bus.list_names(): # For Gnome
+            ssProxy = bus.get_object('org.gnome.ScreenSaver', '/org/gnome/ScreenSaver')
+        elif 'org.freedesktop.ScreenSaver' in bus.list_names(): # For KDE and others
+            ssProxy = bus.get_object('org.freedesktop.ScreenSaver', '/ScreenSaver')
+        else:
+            errMsg = "Error: couldn't find bus to allow inhibiting of the screen saver.\n\n" \
+                "Please visit the web-site listed in the 'About' dialog of this application " \
+                "and check for a newer version of the software."
+            errDlg = gtk.MessageDialog(type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_OK, message_format=errMsg)
+            errDlg.run()
+            errDlg.destroy()
+            sys.exit(2)
 
-#Simulates a click to activate caffeine, then runs activation()
-#after enough time has passed. Runs in another thread, so the script
-#can continue anyway.
+        screenSaverCookie = ssProxy.Inhibit("Caffeine", "User has requested that Caffeine disable the screen saver")
+        sleepPrevented = True
+        print "Caffiene is now preventing powersaving modes and screensaver activation"
+
+# Simulates a click to activate caffeine, then runs activation()
+# after enough time has passed. Runs in another thread, so the script
+# can continue anyway.
 def timedActivation(self, seconds):
-    global sleepPrevented, statusIcon
+    global sleepPrevented, statusIcon, timer
+    print "User has requested timed activation; powersaving will revert to normal after " + str(seconds) + " seconds"
     if sleepPrevented == False:
         sleepPreventionPressed(statusIcon)
+    if timer != None:
+    	print "Cancelling the previous 'timed activation' timer (was set for " + str(timer.interval) + " seconds)"
+    	timer.cancel()
     timer = threading.Timer(seconds, activation)
     timer.start()
-    
+
 def activation():
-    global sleepPrevented, statusIcon
+    global sleepPrevented, statusIcon, timer
+    print "Timed activation period has expired (" + str(timer.interval) + " seconds); Caffeine will now deactivate"
+    timer = None
     if sleepPrevented == True:
         sleepPreventionPressed(statusIcon)
 
 def main():
     global statusIcon
     statusIcon = gtk.StatusIcon()
-    
-    #Creating submenu
+
+    # Creating submenu
     submenu = gtk.Menu()
     menuItem = gtk.MenuItem(label="5 minutes")
     menuItem.connect('activate', timedActivation, 300.0)
@@ -159,7 +172,7 @@ def main():
     menuItem = gtk.MenuItem(label="3 hours")
     menuItem.connect('activate', timedActivation, 9800.0)
     submenu.append(menuItem)
-    
+
     menu = gtk.Menu()
     menuItem = gtk.MenuItem(label="Activate for")
     menuItem.set_submenu(submenu)
@@ -182,4 +195,4 @@ def main():
         quitCaffeine()
 
 if __name__ == "__main__":
-	main()
+    main()
