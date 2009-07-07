86de3e4abcd874879f8eafea472750e10145843d
Brad Smith <>
2009-07-07 09:00:59 -0400

    Updated the copyright header to include Tommy, which is to be expected, given his great contributions.

--- caffeine.py
+++ caffeine.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2009 Brad Smith
+# Copyright © 2009 Brad Smith, Tommy Brunn
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
