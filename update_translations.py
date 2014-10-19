09977d89e21ae3b96fe842377037dfb93a316bff
Hugo Osvaldo Barrera <hugo@barrera.io>
2014-10-19 00:10:40 -0300

    Fix extra parenthesis.

--- update_translations.py
+++ update_translations.py
@@ -5,7 +5,7 @@ version = open("VERSION").read()
 version = version.strip()
 print(version)
 
-print((subprocess.getoutput("python ./generate_pot.py . caffeine " + version +
+print(subprocess.getoutput("python ./generate_pot.py . caffeine " + version +
       " translations/caffeine.pot"))
 
 print(subprocess.getoutput("python ./compile_translations.py caffeine" +
