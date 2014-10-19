8eeae036e6b4e1de367b358a5c3abe6e8a03d6fc
Hugo Osvaldo Barrera <hugo@barrera.io>
2014-10-18 23:30:02 -0300

    Cleaned up setup.py and made it slightly simpler.

--- setup.py
+++ setup.py
@@ -5,23 +5,26 @@ import os
 import shutil
 
 
-def main():
+if __name__ == "__main__":
 
-    SHARE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                              "share")
+    script_path = os.path.abspath(__file__)
+    share_path = os.path.join(os.path.dirname(script_path), "share")
 
     data_files = []
 
-    for path, dirs, files in os.walk(SHARE_PATH):
-        data_files.append(tuple((path.replace(SHARE_PATH, "share", 1),
-                                 [os.path.join(path, file)
-                                     for file in files])))
+    for path, dirs, files in os.walk(share_path):
+        clean_path = path.replace(share_path, "share", 1)
+        data_files.append(tuple((clean_path, [os.path.join(path, file)
+                                              for file in files])))
 
     desktop_name = "taurine.desktop"
     desktop_file = os.path.join("share", "applications", desktop_name)
+
     autostart_dir = os.path.join("etc", "xdg", "autostart")
+
     if not os.path.exists(autostart_dir):
         os.makedirs(autostart_dir)
+
     shutil.copy(desktop_file, autostart_dir)
     data_files.append(tuple(("/" + autostart_dir,
                              [os.path.join(autostart_dir, desktop_name)])))
@@ -33,12 +36,22 @@ def main():
           mode.""",
           author="The Caffeine Developers",
           author_email="hugo@barrera.io",
+          maintainer="Hugo Osvaldo Barrera",
+          maintainer_email="hugo@barrera.io",
           url="https://github.com/hobarrera/taurine",
           packages=["caffeine"],
           data_files=data_files,
-          scripts=[os.path.join("bin", "caffeine"),
-                   os.path.join("bin", "taurine")]
-          )
+          scripts=["bin/caffeine", "bin/taurine"],
+          classifiers=[
+              'Development Status :: 5 - Production/Stable',
+              'Environment :: X11 Applications',
+              'Environment :: X11 Applications :: GTK',
+              'Intended Audience :: End Users/Desktop',
+              'License :: OSI Approved :: GNU General Public License v3',
+              'Operating System :: POSIX',
+              'Operating System :: POSIX :: BSD',
+              'Operating System :: POSIX :: Linux',
+              'Programming Language :: Python :: 3',
+              ]
 
-if __name__ == "__main__":
-    main()
+          )
