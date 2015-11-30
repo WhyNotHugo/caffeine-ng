61a525976ec3c92f07e5c7b22fb8fb69dd89ef1d
Hugo Osvaldo Barrera <hugo@barrera.io>
2015-11-29 21:45:39 -0300

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -21,37 +21,41 @@ def get_data_files():
     return data_files
 
 
+def get_description():
+    return """A status bar application able to temporarily prevent the
+activation of both the screensaver and the "sleep" powersaving mode."""
+
+
 if __name__ == "__main__":
-    setup(name="caffeine-ng",
-          use_scm_version={
-              'version_scheme': 'post-release',
-              'write_to': 'caffeine/version.py',
-          },
-          description="""A status bar application able to temporarily prevent
-          the activation of both the screensaver and the "sleep" powersaving
-          mode.""",
-          author="The Caffeine Developers",
-          author_email="hugo@barrera.io",
-          maintainer="Hugo Osvaldo Barrera",
-          maintainer_email="hugo@barrera.io",
-          url="https://github.com/hobarrera/caffeine-ng",
-          packages=find_packages(),
-          data_files=get_data_files(),
-          entry_points={
-              'gui_scripts': [
-                  'caffeine = caffeine.main:main'
-              ]
-          },
-          classifiers=[
-              'Development Status :: 5 - Production/Stable',
-              'Environment :: X11 Applications',
-              'Environment :: X11 Applications :: GTK',
-              'Intended Audience :: End Users/Desktop',
-              'License :: OSI Approved :: GNU General Public License v3',
-              'Operating System :: POSIX',
-              'Operating System :: POSIX :: BSD',
-              'Operating System :: POSIX :: Linux',
-              'Programming Language :: Python :: 3',
-              ],
-          setup_requires=['setuptools_scm'],
-          )
+    setup(
+        name="caffeine-ng",
+        use_scm_version={
+            'version_scheme': 'post-release',
+            'write_to': 'caffeine/version.py',
+        },
+        description=get_description(),
+        author="The Caffeine Developers",
+        author_email="hugo@barrera.io",
+        maintainer="Hugo Osvaldo Barrera",
+        maintainer_email="hugo@barrera.io",
+        url="https://github.com/hobarrera/caffeine-ng",
+        packages=find_packages(),
+        data_files=get_data_files(),
+        entry_points={
+            'gui_scripts': [
+                'caffeine = caffeine.main:main'
+            ]
+        },
+        classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: X11 Applications',
+          'Environment :: X11 Applications :: GTK',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: GNU General Public License v3',
+          'Operating System :: POSIX',
+          'Operating System :: POSIX :: BSD',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 3',
+        ],
+        setup_requires=['setuptools_scm'],
+    )
